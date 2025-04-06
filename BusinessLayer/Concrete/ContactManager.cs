d61601f538a3c3fe5c7578a02c1ba4f3743183f6
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-04-06 23:33:04 +0300

    Video 42 (Contact Sayfası)

---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class ContactManager : IContactService
+	{
+		IContactDAL _contactDal;
+
+		public ContactManager(IContactDAL contactDal)
+		{
+			_contactDal = contactDal;
+		}
+
+		public void ContactAdd(Contact contact)
+		{
+			_contactDal.Insert(contact);
+		}
+	}
+}
