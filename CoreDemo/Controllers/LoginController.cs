08d4a0e8116777e59aa8de9a39fee755ebcd6485
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-04-08 20:51:39 +0300

    Video 46 (Session ile Oturum Yönetimi)

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
@@ -9,6 +11,23 @@ namespace CoreDemo.Controllers
 		public IActionResult Index()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		[AllowAnonymous]
+		public IActionResult Index(Writer p)
+		{
+			Context c = new Context();
+			var datavalue= c.Writers.FirstOrDefault(x=>x.WriterMail==p.WriterMail && x.WriterPassword == p.WriterPassword);
+			if (datavalue != null)
+			{
+				HttpContext.Session.SetString("username", p.WriterMail);
+				return RedirectToAction("Index", "Writer");
+			}
+			else
+			{
+				return View();
+			}
 		}
 	}
 }
