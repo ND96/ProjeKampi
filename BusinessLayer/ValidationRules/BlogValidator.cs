8fc89b48b7f46c9707807a935a3b9139ca828eea
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-04-11 16:43:38 +0300

    Video 55 (Yazarın Yeni Blog Eklemesi)

---
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class BlogValidator:AbstractValidator<Blog>
+    {
+        public BlogValidator() 
+        {
+            RuleFor(x => x.BlogTitle).NotEmpty().WithMessage("Blog Başlığını boş geçemezsiniz!");
+            RuleFor(x => x.BlogContent).NotEmpty().WithMessage("Blog İçeriğini boş geçemezsiniz!");
+            RuleFor(x => x.BlogImage).NotEmpty().WithMessage("Blog Görselini boş geçemezsiniz!");
+            RuleFor(x => x.BlogTitle).MaximumLength(150).WithMessage("Lütfen 150 karakteri geçmeyiniz");
+            RuleFor(x => x.BlogTitle).MinimumLength(5).WithMessage("Lütfen 4 karakterden fazla giriniz");
+        }
+    
+    }
+}
