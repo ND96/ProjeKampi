04900c3774fd19e07eaf6bb5bfa5e4f0f921ecf9
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-08-18 15:05:51 +0300

    Video 73 (Profil Fotoğrafını Dosyadan ekleme)

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 //using System.ComponentModel.DataAnnotations;
 using FluentValidation.Results;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using CoreDemo.Models;
 
 namespace CoreDemo.Controllers
 {
@@ -76,6 +78,36 @@ namespace CoreDemo.Controllers
 				}
 			}
 			return View();
+		}
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd() 
+		{ 
+			return View();
+		}
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(AddProfileImage p)
+		{
+			Writer w = new Writer();	
+			if (p.WriterImage != null)
+			{
+				var extension =Path.GetExtension(p.WriterImage.FileName);
+				var newimagename = Guid.NewGuid()+extension;
+				var location =Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newimagename);
+				var stream =new FileStream(location,FileMode.Create);
+				p.WriterImage.CopyTo(stream);
+				w.WriterImage=newimagename;
+			}
+			w.WriterMail=p.WriterMail;
+			w.WriterName = p.WriterName;
+			w.WriterPassword = p.WriterPassword;
+			w.WriterStatus = true;
+			w.WriterAbout	=p.WriterAbout;
+			wm.TAdd(w);
+			return RedirectToAction("Index","Dashboard");
 		}
 
     }
