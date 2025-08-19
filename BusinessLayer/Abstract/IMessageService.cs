eff0c7ea4db191ff2dcbe464fc341a5d058d1e73
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-08-19 13:48:23 +0300

    Video 77 (Mesaj Sınıfı 2)

--- BusinessLayer/Abstract/IMessageService.cs
+++ BusinessLayer/Abstract/IMessageService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Abstract
 {
-    internal interface IMessageService
+    public interface IMessageService:IGenericService<Message>
     {
+        List<Message> GetInboxListByWriter(string p);
     }
 }
