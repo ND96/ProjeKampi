a68004124e112133cd1af449d7e74348ecc8ba69
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-08-19 10:21:10 +0300

    Video 75 (Notifications Listelenmesi)

--- EntityLayer/Concrete/Notification.cs
+++ EntityLayer/Concrete/Notification.cs
@@ -17,5 +17,6 @@ namespace EntityLayer.Concrete
         public string NotificationDetails { get; set; }
         public DateTime NotificationDate { get; set; }
         public  bool NotificationStatus { get; set; }
+        public string NotificationColor { get; set; }
     }
 }
