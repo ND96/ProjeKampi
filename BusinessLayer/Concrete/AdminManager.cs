03cf4264d491e88a0f8ddb40f3535d05a0bfd692
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-08-25 15:34:13 +0300

    Video 102 (İstatistik 3)

---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager :IAdminService
+    {
+        IAdminDAL _admindal;
+
+        public AdminManager(IAdminDAL admindal)
+        {
+            _admindal = admindal;
+        }
+
+        public List<Admin> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Admin TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
-    public class Statistic4:ViewComponent
+    public class Statistic4 : ViewComponent
     {
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.v1 = c.Admins.Where(x => x.AdminID == 2).Select(x => x.Name).FirstOrDefault();
+            ViewBag.v2 = c.Admins.Where(x => x.AdminID == 2).Select(x => x.ImageURL).FirstOrDefault();
+            ViewBag.v3 = c.Admins.Where(x => x.AdminID == 2).Select(x => x.ShortDescription).FirstOrDefault();
+
             return View();
         }
     }
