eaca762b101ff9688893ca6bc3841d8a95163245
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-08-26 10:06:26 +0300

    Video 103(Api Üzerinden hava durumu bilgisi çekme)

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +15,13 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 =c.Contacts.Count();
             ViewBag.v3 =c.Comments.Count();
+
+            string api = "d23f63d2ee4573b5fa069dcf36458261";
+            string city = "İstanbul";
+
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=" + city+ "&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document= XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
