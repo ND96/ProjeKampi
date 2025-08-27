0297b34195264bc5199011d8fda44ffcbe7b733b
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-08-27 15:23:31 +0300

    Video  114(Apilere Giriş)

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Server=.;Database=CoreBlogDb;integrated security=true");
+
+            //optionsBuilder.UseSqlServer("Server=.;Database=CoreBlogDb;User Id=sa;Password=1234;TrustServerCertificate=true");
+
+            optionsBuilder.UseSqlServer("Server=.;Database=CoreBlogApiDb; Trusted_Connection=True;TrustServerCertificate=true");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
