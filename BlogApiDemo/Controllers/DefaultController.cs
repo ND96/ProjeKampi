3630c0b30258cc4e899ac0a2a1d0b9732f8de2e8
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-08-28 11:29:46 +0300

    Video 118 (Api ile veri getirme)

--- BlogApiDemo/Controllers/DefaultController.cs
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -23,5 +23,20 @@ namespace BlogApiDemo.Controllers
             c.SaveChanges();
             return Ok();
         }
+
+        [HttpGet("id")]
+        public IActionResult EmployeeGet(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(employee);
+            }
+        }
     }
 }
