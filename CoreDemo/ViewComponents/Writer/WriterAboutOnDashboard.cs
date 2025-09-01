1b4f5f8a08565d780097f7d08bf5e7e26353225c
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-09-01 16:34:30 +0300

    Video 126 (Identity ile giriş yapan kullanıcının bilgilerinin getirilmesi)

--- CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -8,10 +8,13 @@ namespace CoreDemo.ViewComponents.Writer
     public class WriterAboutOnDashboard :ViewComponent
     {
         WriterManager writerManager = new WriterManager(new EfWriterRepository());
+
         Context c = new Context();
         public IViewComponentResult Invoke()
         {
-            var usermail = User.Identity.Name;        
+            var username  = User.Identity.Name;   
+            ViewBag.veri=username;
+            var usermail=c.Users.Where(X=>X.UserName==username).Select(y=>y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
             var values = writerManager.GetWriterByID(writerID);
             return View(values);
