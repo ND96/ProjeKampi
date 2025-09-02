83003d50a824aa1f32cfbdfc930c7ed08df3942f
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-09-02 16:54:09 +0300

    Video 129 (Yazar Bilgilerini Identity e bağlama)

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -9,117 +9,125 @@ using FluentValidation.Results;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using CoreDemo.Models;
 using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 
 namespace CoreDemo.Controllers
 {
 
-	public class WriterController : Controller
-	{
+    public class WriterController : Controller
+    {
 
-		WriterManager wm = new WriterManager(new EfWriterRepository());
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        UserManager userManager = new UserManager(new EfUserRepository());
+        private readonly UserManager<AppUser> _userManager;
 
-		[Authorize]
-		public IActionResult Index()
-		{
-			var usermail = User.Identity.Name;
-			ViewBag.usermail=usermail;
-			Context c = new Context();
-			var writerName = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterName).FirstOrDefault();
-			ViewBag.writerName = writerName;
-			return View();
-		}
+        public WriterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
-		public IActionResult WriterProfile ()
-		{
-			return View();
-		}
+        [Authorize]
+        public IActionResult Index()
+        {
+            var usermail = User.Identity.Name;
+            ViewBag.usermail = usermail;
+            Context c = new Context();
+            var writerName = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterName).FirstOrDefault();
+            ViewBag.writerName = writerName;
+            return View();
+        }
 
-		public IActionResult WriterMail()
-		{
-			return View();
-		}
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
 
-
-		[AllowAnonymous]
-		public IActionResult Test()
-		{
-			return View();
-		}
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
 
 
-		[AllowAnonymous]
-		public PartialViewResult WriterNavbarPartila()
-		{
-			return PartialView();
-		}
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
 
-		[AllowAnonymous]
-		public PartialViewResult WriterFooterPartial()
-		{
-			return PartialView(); 
-		}
 
-		
-		[HttpGet]
-		public IActionResult WriterEditProfile()
-		{
-			Context c = new Context();
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartila()
+        {
+            return PartialView();
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
+
+
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
+            Context c = new Context();
             var username = User.Identity.Name;
-			var usermail= c.Users. Where(x=>x.UserName==username).Select(y=>y.Email).FirstOrDefault();
-            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
-            var writervalues = wm.TGetById(writerID);
-			return View(writervalues);
-		}
-		
-		[HttpPost]
-		public IActionResult WriterEditProfile(Writer p)
-		{  
-			WriterValidator wl = new WriterValidator();
-			ValidationResult results = wl.Validate(p);
-			if (results.IsValid)
-			{
-				wm.TUpdate(p);
-				return RedirectToAction ("Index","Dashboard");
-			}
-			else
-			{
-				foreach(var item in results.Errors)
-				{
-					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
-				}
-			}
-			return View();
-		}
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var id = c.Users.Where(x => x.Email == usermail).Select(y => y.Id).FirstOrDefault();
+            var values = userManager.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer p)
+        {
+            WriterValidator wl = new WriterValidator();
+            ValidationResult results = wl.Validate(p);
+            if (results.IsValid)
+            {
+                wm.TUpdate(p);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
 
         [AllowAnonymous]
         [HttpGet]
-        public IActionResult WriterAdd() 
-		{ 
-			return View();
-		}
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
 
         [AllowAnonymous]
         [HttpPost]
         public IActionResult WriterAdd(AddProfileImage p)
-		{
-			Writer w = new Writer();	
-			if (p.WriterImage != null)
-			{
-				var extension =Path.GetExtension(p.WriterImage.FileName);
-				var newimagename = Guid.NewGuid()+extension;
-				var location =Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newimagename);
-				var stream =new FileStream(location,FileMode.Create);
-				p.WriterImage.CopyTo(stream);
-				w.WriterImage=newimagename;
-			}
-			w.WriterMail=p.WriterMail;
-			w.WriterName = p.WriterName;
-			w.WriterPassword = p.WriterPassword;
-			w.WriterStatus = true;
-			w.WriterAbout	=p.WriterAbout;
-			wm.TAdd(w);
-			return RedirectToAction("Index","Dashboard");
-		}
+        {
+            Writer w = new Writer();
+            if (p.WriterImage != null)
+            {
+                var extension = Path.GetExtension(p.WriterImage.FileName);
+                var newimagename = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newimagename);
+                var stream = new FileStream(location, FileMode.Create);
+                p.WriterImage.CopyTo(stream);
+                w.WriterImage = newimagename;
+            }
+            w.WriterMail = p.WriterMail;
+            w.WriterName = p.WriterName;
+            w.WriterPassword = p.WriterPassword;
+            w.WriterStatus = true;
+            w.WriterAbout = p.WriterAbout;
+            wm.TAdd(w);
+            return RedirectToAction("Index", "Dashboard");
+        }
 
     }
 }
