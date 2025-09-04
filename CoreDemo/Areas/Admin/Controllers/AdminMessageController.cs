2d3c6dda56ced4c41ad5a0cf35e1eaceeebd9276
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-09-04 16:24:56 +0300

    Video 137(Admin Blog Listesi)

--- CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers
@@ -26,9 +27,24 @@ namespace CoreDemo.Areas.Admin.Controllers
             var values = mm.GetSendboxListByWriter(writerID);
             return View(values);
         }
+
+        [HttpGet]
         public IActionResult ComposeMessage()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult ComposeMessage(Message2 p)
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            p.SenderID = writerID;
+            p.ReceiverID = 2;
+            p.MessageDate=Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.MessageStatus = true;
+            mm.TAdd(p);            
+            return RedirectToAction("SendBox");
         }
     }
 }
