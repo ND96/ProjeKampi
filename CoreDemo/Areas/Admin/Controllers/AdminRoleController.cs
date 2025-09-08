9e547547f9575b8ce35e66ee0be98eb4f31be000
Nihat Demirezen <nihatdemirezen1907@gmail.com>
2025-09-08 16:58:53 +0300

    Video 143 (Rol atama Rol Kaldırma işlemi)

--- CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
@@ -23,11 +23,11 @@ namespace CoreDemo.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            var values = _roleManager.Roles.ToList();   
+            var values = _roleManager.Roles.ToList();
             return View(values);
         }
         [HttpGet]
-        public IActionResult AddRole() 
+        public IActionResult AddRole()
         {
             return View();
         }
@@ -40,27 +40,27 @@ namespace CoreDemo.Areas.Admin.Controllers
                 {
                     Name = model.name
                 };
-            
+
                 var result = await _roleManager.CreateAsync(role);
-                if (result.Succeeded) 
+                if (result.Succeeded)
                 {
                     return RedirectToAction("Index");
                 }
-                foreach ( var item in result.Errors ) 
+                foreach (var item in result.Errors)
                 {
-                    ModelState.AddModelError("",item.Description);
+                    ModelState.AddModelError("", item.Description);
                 }
             }
             return View(model);
         }
         [HttpGet]
-        public IActionResult UpdateRole(int id )
+        public IActionResult UpdateRole(int id)
         {
-            var values = _roleManager.Roles.FirstOrDefault(x=>x.Id==id);
+            var values = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
             RoleUpdateViewModel model = new RoleUpdateViewModel
             {
                 Id = values.Id,
-                name=values.Name
+                name = values.Name
             };
             return View(model);
         }
@@ -70,7 +70,7 @@ namespace CoreDemo.Areas.Admin.Controllers
             var values = _roleManager.Roles.Where(x => x.Id == model.Id).FirstOrDefault();
 
             values.Name = model.name;
-            var result=await _roleManager.UpdateAsync(values);
+            var result = await _roleManager.UpdateAsync(values);
             if (result.Succeeded)
             {
                 return RedirectToAction("Index");
@@ -78,11 +78,11 @@ namespace CoreDemo.Areas.Admin.Controllers
             return View(model);
         }
 
-        public async Task<IActionResult> DeleteRole(int id) 
+        public async Task<IActionResult> DeleteRole(int id)
         {
             var values = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
             var result = await _roleManager.DeleteAsync(values);
-            if (result.Succeeded) 
+            if (result.Succeeded)
             {
                 return RedirectToAction("Index");
             }
@@ -91,11 +91,11 @@ namespace CoreDemo.Areas.Admin.Controllers
 
         public IActionResult UserRoleList()
         {
-            var values =_userManager.Users.ToList();
+            var values = _userManager.Users.ToList();
             return View(values);
         }
         [HttpGet]
-        public async Task<IActionResult> AssignRole(int id )
+        public async Task<IActionResult> AssignRole(int id)
         {
             var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
             var roles = _roleManager.Roles.ToList();
@@ -108,13 +108,32 @@ namespace CoreDemo.Areas.Admin.Controllers
             foreach (var item in roles)
             {
                 RoleAssignViewModel m = new RoleAssignViewModel();
-                m.RoleID= item.Id;
+                m.RoleID = item.Id;
                 m.Name = item.Name;
                 m.Exists = userRoles.Contains(item.Name);
                 model.Add(m);
             }
             return View(model);
 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(List<RoleAssignViewModel> model)
+        {
+            var userid = (int)TempData["UserId"];
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == userid);
+            foreach (var item in model)
+            {
+                if (item.Exists)
+                {
+                    await _userManager.AddToRoleAsync(user, item.Name);
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.Name);
+                }
+            }
+            return RedirectToAction("UserRoleList");
         }
     }
 }
